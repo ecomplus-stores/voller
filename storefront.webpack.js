620746cf45c38567cdd2444387bd8559d7076311
matheusgnreis <matheus@e-com.club>
2023-07-24 14:49:12 -0300

    chore: grid de avaliacoes

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -6,7 +6,8 @@ module.exports = () => ({
   resolve: {
     alias: {
       './html/PaymentOption.html': path.resolve(__dirname, 'template/js/custom-js/html/PaymentOption.html'),
-      './js/PaymentOption.js': path.resolve(__dirname, 'template/js/custom-js/js/PaymentOption.js')
+      './js/PaymentOption.js': path.resolve(__dirname, 'template/js/custom-js/js/PaymentOption.js'),
+      './base-config': path.resolve(__dirname, 'template/js/netlify-cms/base-config')
       //'./js/SearchEngine.js': path.resolve(__dirname, 'template/js/custom-js/js/SearchEngine.js')
       //'./js/Checkout.js': path.resolve(__dirname, 'template/js/views/js/Checkout.js')
     }
