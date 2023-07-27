4a536d9e55acc0fc652fa8179d9924a29a819057
matheusgnreis <matheus@e-com.club>
2023-07-27 01:59:40 -0300

    chore: set sku to search and remove border blue

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -1,10 +1,13 @@
 // storefront.webpack.js
-
 const path = require('path')
+
+const dirSearchAlias = path.resolve(__dirname, 'template/js/lib/search-engine')
+const pathDslAlias = path.resolve(dirSearchAlias, 'dsl')
 
 module.exports = () => ({
   resolve: {
     alias: {
+      './methods/set-search-term': path.resolve(dirSearchAlias, 'set-search-term'),
       './html/PaymentOption.html': path.resolve(__dirname, 'template/js/custom-js/html/PaymentOption.html'),
       './js/PaymentOption.js': path.resolve(__dirname, 'template/js/custom-js/js/PaymentOption.js'),
       './base-config': path.resolve(__dirname, 'template/js/netlify-cms/base-config')
